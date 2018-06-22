8d02086366c7d8f7370d4f07cf3c6d29631a6749
Phillip-Juan Van Der Berg <pjvanderberg@ZAILAB.COM>
2018-06-22 14:16:14 +0200

    play with virtual dom

--- src/views/desktop/desktop.ts
+++ src/views/desktop/desktop.ts
@@ -5,25 +5,32 @@ import { Constants } from './../../constants';
 import { IMessage } from '../../services/eventing';
 import './desktop.css';
 // constants
-const logger = new Logger('PageOne');
+const logger = new Logger('Desktop');
 
-export class PageOne extends Base {
+export class Desktop extends Base {
 
   public static ICON_SIZES: any = { 
-    SMALL: { CLASS: 'small', DIMENSION: 45 }, MEDIUM: { CLASS: 'medium', DIMENSION: 85 }, LARGE: { CLASS: 'large', DIMENSION: 125 }
+    SMALL: { CLASS: 'small', DIMENSION: 48 }, MEDIUM: { CLASS: 'medium', DIMENSION: 85 }, LARGE: { CLASS: 'large', DIMENSION: 125 }
   };
-  private iconSize: string = PageOne.ICON_SIZES.SMALL.CLASS;
-  private iconDimension = PageOne.ICON_SIZES.SMALL.DIMENSION;
+  private iconSize: string = Desktop.ICON_SIZES.SMALL.CLASS;
+  private iconDimension = Desktop.ICON_SIZES.SMALL.DIMENSION;
   private rows: number = 3;
   private columns: number = 5;
 
   constructor() {
     super();
 
-    this.rows = Math.floor(window.screen.availHeight / this.iconDimension);
-    logger.info(' >>>>> this.rows ', this.rows);
-    this.columns = Math.floor(window.screen.availWidth / this.iconDimension);
+    var w = window,
+    d = document,
+    e = d.documentElement,
+    g = document.body,
+    x = w.innerWidth || e.clientWidth || g.clientWidth,
+    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+    logger.info(' >>>>> window.screen ', x + ' × ' + y);
+    this.columns = Math.floor(x / this.iconDimension);
     logger.info(' >>>>> this.columns ', this.columns);
+    this.rows = Math.floor(y / this.iconDimension);
+    logger.info(' >>>>> this.rows ', this.rows);
 
     super.subscribe(Constants.UPDATED.TEST, (data: IMessage) => {
       logger.debug(' ::>> handling message >>>>> ', data);
@@ -35,15 +42,16 @@ export class PageOne extends Base {
   }
 
   protected attached(): void {
-    let container: HTMLElement = document.querySelector('.js-desktop');
-    logger.info(' >>>>> window.screen.availHeight ', window.screen.availHeight);
-    logger.info(' >>>>> window.screen.availWidth ', window.screen.availWidth);
+    // let container: HTMLElement = document.querySelector('.js-desktop');
+    // logger.info(' >>>>> window.screen ', window.screen);
+    // logger.info(' >>>>> window.screen.availHeight ', window.screen.availHeight);
+    // logger.info(' >>>>> window.screen.availWidth ', window.screen.availWidth);
   }
 
   private setDimension(size: string): void {
     try {
-      this.iconSize = PageOne.ICON_SIZES[size.toUpperCase()].CLASS;
-      this.iconDimension = PageOne.ICON_SIZES[size.toUpperCase()].DIMENSION;
+      this.iconSize = Desktop.ICON_SIZES[size.toUpperCase()].CLASS;
+      this.iconDimension = Desktop.ICON_SIZES[size.toUpperCase()].DIMENSION;
     } catch(e) {
       logger.error(`Failed to chane desktop icon dimentions to ${size} due to cause:`, e);
     }
