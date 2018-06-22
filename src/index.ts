4d75f8de2931e0c9e04bd026fd67b18f03033aca
Phillip-Juan van der Berg <flaapjan@zailab.com>
2018-06-22 17:19:44 +0200

    render templates sucessfully

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,7 @@ const logger: Logger = new Logger('Index');
 var h = require('virtual-dom/h');
 var diff = require('virtual-dom/diff');
 var patch = require('virtual-dom/patch');
+var VText = require("virtual-dom/vnode/vtext")
 var createElement = require('virtual-dom/create-element');
 
 ////////////////////////////////////////////////////////
@@ -28,9 +29,9 @@ import { RequestService } from './services/request-service';
 var parser = require('parse5');
 
 const DOC_TYPES = { DOCUMENT: '#document', HTML: 'html', HEAD: 'head', TEMPLATE: 'template' };
+const HASH_TYPES: any = ['#text'];
 
 function getContent(dom: any): any {
-  logger.info(' ::>> getContent >>>> ', dom);
   try {
     switch(dom.nodeName) {
       case DOC_TYPES.DOCUMENT: return getContent(dom.childNodes[0]);
@@ -48,37 +49,23 @@ function getContent(dom: any): any {
 }
 
 async function structureTemplate(domNodes: any[], isChildren?: boolean): Promise<any> {
-  let _data: any = {};
+  let _data: any = isChildren ? null : [];
   let _children: any[] = isChildren ? [] : null;
   
   for(let node of domNodes) {
-    let identity = generateTagIdentity(node);
-    let children: any = await getChildNodes(node);
+    let identity: string = generateTagIdentity(node);
     let value: any = await getNodeValue(node);
-    // logger.info(' ::>> mapping >>>>> ', { identity, children, value });
-    if(isChildren) {
-      let obj: any = {};
-      // obj[generateTagIdentity(node)]  = value || children;
-      logger.info(' ::>> pushing child ', h(generateTagIdentity(node), value || children));
-      _children.push(h(generateTagIdentity(node), value || children));
-    } else {
-      // _data[generateTagIdentity(node)] = value || children;
-      logger.info(' ::>> creating entity ', h(generateTagIdentity(node), value || children));
-      _data = h(generateTagIdentity(node), value || children);
-    }
+    let children: any = await getChildNodes(node);
 
-    // _data = h(generateTagIdentity(node), value || children);
+    if(isChildren) {
+      _children.push(HASH_TYPES.includes(identity) ? new VText(value) : h(identity, value || children));
+    } else {
+      _data.push(HASH_TYPES.includes(identity) ? new VText(value) : h(identity, value || children));
+    }
   }
-  logger.exclaim(' :> _data > ', _data);
-  logger.exclaim(' :> _children > ', _children);
   
   return _children || _data;
 }
-
-// var tree = h('div.foo#some-id', [
-//   h('span', 'some text'),
-//   h('input', { type: 'text', value: 'foo' })
-// ])
 
 async function getChildNodes(node: any): Promise<any[]> {
   if(node.childNodes && node.childNodes.length > 0) {
@@ -111,50 +98,15 @@ function generateTagIdentity(node: any): string {
 }
 
 async function buildTemplate(templateURL: string) {
-  
   let templateString = await RequestService.fetch(templateURL).asHtml();
-  logger.debug(' ::>> template >>>>> ', templateString);
   let dom = parser.parse(templateString);
-  logger.debug(' ::>> template >>>>> ', dom);
   let templateContent = await getContent(dom);
-  logger.debug(' ::>> templateContent >>>>> ', templateContent);
-  let tree = await structureTemplate(templateContent);
-  console.log(' ::>> tree >>>>>> ', tree);
-  // var tree = render(count);
-  var rootNode = createElement(tree);
-  document.body.appendChild(rootNode);
+  let forest = await structureTemplate(templateContent);
+
+  for(let tree of forest) {
+    var rootNode = createElement(tree);
+    document.body.appendChild(rootNode);
+  }
 }
 
 buildTemplate('views/pagetwo/page-two');
-
-///////////////////////////////////////////////////////////
- 
-// 1: Create a function that declares what the DOM should look like
-function render(count: number)  {
-  return h('div', {
-    style: {
-      textAlign: 'center',
-      lineHeight: (100 + count) + 'px',
-      border: '1px solid red',
-      width: (100 + count) + 'px',
-      height: (100 + count) + 'px'
-    }
-  }, [String(count)]);
-}
-
-// 2: Initialise the document
-// var count = 0;      // We need some app data. Here we just store a count.
- 
-// var tree = render(count);               // We need an initial tree
-// var rootNode = createElement(tree);     // Create an initial root DOM node ...
-// document.body.appendChild(rootNode);    // ... and it should be in the document
- 
-// // 3: Wire up the update logic
-// setInterval(function () {
-//   count++;
-
-//   var newTree = render(count);
-//   var patches = diff(tree, newTree);
-//   rootNode = patch(rootNode, patches);
-//   tree = newTree;
-// }, 1000);
